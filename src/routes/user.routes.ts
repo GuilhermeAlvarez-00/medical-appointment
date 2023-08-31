aa172830ab7506b600b5709b7ca0ea27f46a724e
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2023-08-31 18:41:35 -0300

    refactor: add route to create speciality

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { createUserController } from "../modules/users/usecases/create-user";
+import { specialityController } from "../modules/speciality/useCases/create-speciality";
 
 const userRouter = Router();
 
 userRouter.post("/users", async (req, res) => {
   await createUserController.handle(req, res);
+});
+
+userRouter.post("/speciality", async (req, res) => {
+  await specialityController.handle(req, res);
 });
 
 export { userRouter };
