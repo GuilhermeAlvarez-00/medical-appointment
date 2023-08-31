280df85fde19bd21dcd52ddf2e1e1da684470d0d
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2023-08-31 15:18:29 -0300

    feat: prisma implementation to user repository

---
+++ src/modules/users/repositories/implementations/user.prisma.repository.ts
@@ -0,0 +1,27 @@
+import { IUserRepository } from "../user.repository";
+import { prismaClient } from "../../../../infra/database/prisma.config";
+import { User } from "@prisma/client";
+
+export class UserPrismaRepository implements IUserRepository {
+  async findByUsername(username: string): Promise<User | undefined> {
+    const user = await prismaClient.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    return user || undefined;
+  }
+
+  async save(data: User): Promise<User> {
+    const user = await prismaClient.user.create({
+      data: {
+        name: data.name,
+        password: data.password,
+        username: data.username,
+      },
+    });
+
+    return user;
+  }
+}
