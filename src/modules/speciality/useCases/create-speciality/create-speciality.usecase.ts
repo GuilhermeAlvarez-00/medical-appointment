cd9d384f43fa7dda78bebbec6d3a7f41cd8f887b
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2023-09-04 16:35:00 -0300

    refactor: adjusting custom error

--- src/modules/speciality/useCases/create-speciality/create-speciality.usecase.ts
+++ src/modules/speciality/useCases/create-speciality/create-speciality.usecase.ts
@@ -1,3 +1,4 @@
+import { CustomError } from "../../../../errors/custom-error";
 import { Speciality } from "../../entities/speciality.entity";
 import { ISpecialityRepository } from "../../repositories/speciality.repository";
 
@@ -10,6 +11,18 @@ export class CreateSpecialityUseCase {
   constructor(private specialityRepository: ISpecialityRepository) {}
   async execute(data: ISpeciality) {
     const speciality = Speciality.create(data);
+
+    const specialityAlreadyExists = await this.specialityRepository.findByName(
+      data.name
+    );
+
+    if (specialityAlreadyExists) {
+      throw new CustomError(
+        "This speciality already exists",
+        400,
+        "SPECIALITY_ALREADY_EXISTS"
+      );
+    }
 
     const specialityCreated = await this.specialityRepository.save(speciality);
 
