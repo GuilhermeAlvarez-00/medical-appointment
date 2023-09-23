66d79ab46aabe1156abeb41c57a125d3b20db1d1
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2023-09-23 15:20:26 -0300

    refactor: return user data in token

--- src/infra/shared/token/jwt.token.ts
+++ src/infra/shared/token/jwt.token.ts
@@ -2,7 +2,7 @@ import { sign, verify } from "jsonwebtoken";
 import { createHmac } from "crypto";
 
 import { User } from "../../../modules/users/entities/user.entity";
-import { IToken } from "./token";
+import { IToken, TokenUser } from "./token";
 
 export class JWTToken implements IToken {
   private ENV_SECRET_KEY = process.env.JWT_SECRET_KEY || "";
@@ -11,30 +11,30 @@ export class JWTToken implements IToken {
     "base64"
   );
 
-  create({ username, isAdmin }: User): string {
+  create({ username, isAdmin, id }: User): string {
     const token = sign(
       {
         user: {
+          id,
           username,
           isAdmin,
         },
       },
       this.HASH_SECRET_KEY,
       {
-        expiresIn: "1min",
+        subject: id,
+        expiresIn: "15min",
       }
     );
 
     return token;
   }
 
-  validate(token: string): boolean {
+  validate(token: string): TokenUser | null {
     try {
-      verify(token, this.HASH_SECRET_KEY);
-
-      return true;
+      return verify(token, this.HASH_SECRET_KEY) as TokenUser;
     } catch (error) {
-      return false;
+      return null;
     }
   }
 }
