69e30a793b0fdf0affd55faa698c5eca96a4ce0e
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2023-09-23 15:17:10 -0300

    refactor: use middleware to check if user is admin

--- src/routes/speciality.routes.ts
+++ src/routes/speciality.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { specialityController } from "../modules/speciality/useCases/create-speciality";
 import { ensureAuthentication } from "../infra/shared/http/middleware/ensure-authenticate.middleware";
+import { ensureAdmin } from "../infra/shared/http/middleware/ensure-admin.middlaware";
 
 const specialityRouter = Router();
 
 specialityRouter.post(
   "/specialities",
   ensureAuthentication,
+  ensureAdmin,
   async (req, res) => {
     await specialityController.handle(req, res);
   }
