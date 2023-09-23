69d493222f2a455dc28b96b9d834dad3bbfe7b3c
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2023-09-23 11:39:28 -0300

    feat: middleware to ensure authentication

---
+++ src/infra/shared/http/middleware/ensure-authenticate.middleware.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import { CustomError } from "../../../../errors/custom-error";
+import { JWTToken } from "../../token/jwt.token";
+
+export const ensureAuthentication = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const headerAuth = req.headers.authorization;
+
+  if (!headerAuth) {
+    return res.status(401).json({ message: "Token is missing!" });
+  }
+
+  const [, token] = headerAuth.split(" ");
+
+  if (!token) {
+    return res.status(401).json({ message: "Token is missing!" });
+  }
+
+  const isTokenValid = new JWTToken().validate(token);
+
+  if (isTokenValid) return next();
+
+  return res.status(401).json({
+    message: "Token invalid",
+  });
+};
