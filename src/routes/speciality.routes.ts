d61a56e6f47dd3341a8971bc2d7f80627a5b9386
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2023-09-23 11:39:06 -0300

    refactor: add middleware to ensureAuthentication in routes speciality

--- src/routes/speciality.routes.ts
+++ src/routes/speciality.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { specialityController } from "../modules/speciality/useCases/create-speciality";
+import { ensureAuthentication } from "../infra/shared/http/middleware/ensure-authenticate.middleware";
 
 const specialityRouter = Router();
 
-specialityRouter.post("/specialities", async (req, res) => {
-  await specialityController.handle(req, res);
-});
+specialityRouter.post(
+  "/specialities",
+  ensureAuthentication,
+  async (req, res) => {
+    await specialityController.handle(req, res);
+  }
+);
 
 export { specialityRouter };
