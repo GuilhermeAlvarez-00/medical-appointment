5922eec502d6caa52ba623cd0d505d78a3110ad8
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2023-09-23 15:16:32 -0300

    feat: middleware to check if user id admin

---
+++ src/infra/shared/http/middleware/ensure-admin.middlaware.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from "express";
+import { UserPrismaRepository } from "../../../../modules/users/repositories/implementations/user.prisma.repository";
+
+export const ensureAdmin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const userRepository = new UserPrismaRepository();
+
+  console.log("USERID: ", req.body.userId);
+
+  const user = await userRepository.findById(req.body.userId);
+
+  if (!user) {
+    return res.status(400).json({ message: "User not exist" });
+  }
+
+  if (!user?.isAdmin) {
+    return res.status(401).json({ message: "User not admin" });
+  }
+
+  return next();
+};
