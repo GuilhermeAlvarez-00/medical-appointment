76ddc213f4e14e794bacc218fb7b52a4e9ddbaeb
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2023-11-02 11:55:34 -0300

    refactor: save doctor info in usecase

--- src/modules/doctor/useCases/create-doctor-info/create-doctor-info.usecase.ts
+++ src/modules/doctor/useCases/create-doctor-info/create-doctor-info.usecase.ts
@@ -1,14 +1,17 @@
-import { P } from "vitest/dist/reporters-5f784f42";
 import { CustomError } from "../../../../errors/custom-error";
 import { DoctorInfo, DoctorInfoProps } from "../../entities/doctor-info.entity";
+import { IDoctorInfoRepository } from "../../repositories/doctor-info.repository";
 import { IDoctorRepository } from "../../repositories/doctor.repository";
 
 export type CreateDoctorInfoUseCaseProps = Omit<DoctorInfoProps, "doctorId">;
 
 export class CreateDoctorInfoUseCase {
-  constructor(private doctorRepository: IDoctorRepository) {}
+  constructor(
+    private doctorRepository: IDoctorRepository,
+    private doctorInfoRepository: IDoctorInfoRepository
+  ) {}
   async execute(data: CreateDoctorInfoUseCaseProps, userId: string) {
-    const doctorByUserId = await this.doctorRepository.findBytUserId(userId);
+    const doctorByUserId = await this.doctorRepository.findByUserId(userId);
 
     if (!doctorByUserId) {
       throw new CustomError("Doctor does not exist");
@@ -18,6 +21,8 @@ export class CreateDoctorInfoUseCase {
       ...data,
       doctorId: doctorByUserId.id,
     });
-    return doctorInfo;
+
+    const doctorCreated = await this.doctorInfoRepository.save(doctorInfo);
+    return doctorCreated;
   }
 }
