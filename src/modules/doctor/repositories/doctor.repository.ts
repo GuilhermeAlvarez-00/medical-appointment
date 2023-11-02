63b64b9ebec98805b0f9d7080db2c7ce1313a5f2
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2023-11-02 11:53:40 -0300

    refactor: adjusting name method

--- src/modules/doctor/repositories/doctor.repository.ts
+++ src/modules/doctor/repositories/doctor.repository.ts
@@ -3,6 +3,5 @@ import { Doctor } from "../entities/doctor.entity";
 export interface IDoctorRepository {
   save(doctor: Doctor): Promise<Doctor | null>;
   findByCRM(crm: string): Promise<string | null>;
-  findBytUserId(userId: string): Promise<Doctor | null>;
+  findByUserId(userId: string): Promise<Doctor | null>;
 }
-
