84b18da82382fb958c13a568172b0b11e2f09c57
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2024-07-22 21:35:44 -0300

    refactor: adjusting types of IPatientRepository

--- src/modules/patient/repositories/implementation/patient-prisma.repository.ts
+++ src/modules/patient/repositories/implementation/patient-prisma.repository.ts
@@ -1,9 +1,9 @@
 import { prismaClient } from "../../../../infra/database/prisma.config";
-import { TPatient } from "../../entities/patient.entity";
+import { Patient, TPatient } from "../../entities/patient.entity";
 import { IPatientRepository } from "../patient.repository";
 
 export class PatientPrismaRepository implements IPatientRepository {
-  async save(data: TPatient) {
+  async save(data: Patient): Promise<Patient> {
     const patient = await prismaClient.patient.create({
       data: {
         document: data.document,
@@ -15,10 +15,7 @@ export class PatientPrismaRepository implements IPatientRepository {
     return patient;
   }
 
-  async findByDocumentOrEmail(
-    document: string,
-    email: string
-  ): Promise<TPatient | null> {
+  async findByDocumentOrEmail(document: string, email: string): Promise<TPatient | null> {
     const patient = await prismaClient.patient.findFirst({
       where: {
         OR: [
@@ -33,6 +30,26 @@ export class PatientPrismaRepository implements IPatientRepository {
     });
 
     return patient;
+  }
+
+  async findById(id: string): Promise<Patient | null> {
+    const patient = await prismaClient.patient.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return patient;
+  }
+
+  async findByUserId(userId: string): Promise<Patient | null> {
+    const patient = prismaClient.patient.findUnique({
+      where: {
+        userId,
+      },
+    });
+
+    return patient || null;
   }
 }
 
--- src/modules/patient/repositories/patient.repository.ts
+++ src/modules/patient/repositories/patient.repository.ts
@@ -1,11 +1,10 @@
-import { TPatient } from "../entities/patient.entity";
+import { Patient, TPatient } from "../entities/patient.entity";
 import { TCreatePatientRequest } from "../useCases/create-patient/create-patient.usecase";
 
 export type IPatientRepository = {
-  save: (data: TPatient) => Promise<TPatient>;
-  findByDocumentOrEmail: (
-    document: string,
-    email: string
-  ) => Promise<TPatient | null>;
+  save: (data: Patient) => Promise<Patient>;
+  findByDocumentOrEmail: (document: string, email: string) => Promise<TPatient | null>;
+  findById: (id: string) => Promise<Patient | null>;
+  findByUserId: (userId: string) => Promise<Patient | null>;
 };
 
